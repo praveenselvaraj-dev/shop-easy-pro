4747ece597838ae734b015846709ad4c28b0c5f1
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-07 22:50:29 +0530

    Url Updated

--- src/api/clients/productClient.js
+++ src/api/clients/productClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { setupInterceptors } from "./Interceptors";
 const productClient = axios.create({
-  baseURL: "http://127.0.0.1:8001/api/v1/",
+  baseURL: "https://shop-easy-pro-api-product.onrender.com/api/v1/",
   timeout: 8000
 });
 
