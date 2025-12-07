323649418555f5d7fcdc49978ed381b5c2aa714d
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-07 23:34:34 +0530

    Url Updated for cart

--- src/api/clients/cartClient.js
+++ src/api/clients/cartClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setupInterceptors } from "./Interceptors";
 const cartClient = axios.create({
-  baseURL: "http://127.0.0.1:8002/api/v1/",
+  baseURL: "https://shop-easy-pro-api-cart.onrender.com/api/v1/",
   withCredentials: true,
   timeout: 8000
 });
