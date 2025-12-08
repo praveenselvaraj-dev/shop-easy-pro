3409bbb15c6de5cb85579cd7a7e4abac8cd465ae
Praveen Kumar <Preveenkumar@fabc.global>
2025-12-08 09:37:48 +0530

    Url Updated for all client

--- src/api/clients/adminClient.js
+++ src/api/clients/adminClient.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 import { setupInterceptors } from "./Interceptors";
 const adminClient = axios.create({
-  baseURL: "http://127.0.0.1:8004/api/v1/admin",
+  baseURL: "https://shop-easy-pro-api-admin.onrender.com/api/v1/admin",
   withCredentials: true,
 });
 
 setupInterceptors(adminClient);
-
-
 
 export default adminClient;
--- src/api/clients/orderClient.js
+++ src/api/clients/orderClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const orderClient = axios.create({
-  baseURL: "http://127.0.0.1:8003/api/v1/",
+  baseURL: "https://shop-easy-pro-api-order.onrender.com/api/v1/",
   withCredentials: true,
 });
 
